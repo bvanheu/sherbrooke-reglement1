8f67b2ebc94b17a510d15f4bf9fee9a4217c8fb3
Benjamin Vanheuverzwijn <bvanheu@gmail.com>
2015-07-31 00:41:04 -0400

    Updated the fetch scripts to get the reglement 1 file correctly

--- scripts/fetch_reglement1.sh
+++ scripts/fetch_reglement1.sh
@@ -2,7 +2,7 @@
 
 BASE_URL="http://www.ville.sherbrooke.qc.ca/"
 TODAY=$(date +"%Y_%m_%d")
-REGLEMENT1_URL=$(curl "http://www.ville.sherbrooke.qc.ca/services-municipaux/service-des-affaires-juridiques/reglements/" 2> /dev/null | grep -oh 'fileadmin[a-zA-Z\/.]*reglement1[a-zA-Z\/\._ 0-9-]*')
+REGLEMENT1_URL=$(curl "http://www.ville.sherbrooke.qc.ca/services-municipaux/service-des-affaires-juridiques/reglements/" 2> /dev/null | grep -ioh 'fileadmin[a-zA-Z\/\._ 0-9-]*reglement[ _]n[a-zA-Z\/\._ 0-9-]*')
 REGLEMENT1_FULL_URL="$BASE_URL/$REGLEMENT1_URL"
 REGLEMENT1_FILENAME=$(basename "$REGLEMENT1_FULL_URL")
 SED_LINE='s/’/ /g ; s/[0-9]\{2\}-[0-9]\{2\}-[0-9]\{4\}//g ; s/[0-9]\/[0-9]\/[0-9]\{2\}//g ; s/^[ ]*[0-9]\+$/ /g'
@@ -11,7 +11,12 @@ touch latest_sha1sum.txt
 
 # Download the reglement 1
 echo $REGLEMENT1_FULL_URL
-curl -o "$REGLEMENT1_FILENAME" "$REGLEMENT1_FULL_URL"
+curl -o "$REGLEMENT1_FILENAME" "$REGLEMENT1_FULL_URL" &> /dev/null
+
+if [ ! -f "$REGLEMENT1_FILENAME" ]; then
+    echo "Unable to fetch ${REGLEMENT1_FULL_URL}"
+    exit 1
+fi
 
 # Compute sha1sum
 SHA1SUM=$(sha1sum "$REGLEMENT1_FILENAME" | cut -d ' ' -f 1)
@@ -48,6 +53,7 @@ if [ $? -eq 1 ] ; then
     echo "<p><a href=\"diff/${TODAY}_sherbrooke_reglement1.html\">${TODAY} - sherbrooke reglement 1</a></p>" >> ./index.html
     git add index.html
     git commit -m "Updated to $TODAY"
+    echo "All done, please 'git push' manually"
 else
     # Not a new reglement, destroy.
     rm "$REGLEMENT1_FILENAME"
